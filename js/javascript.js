3999db5c58c19a6793a4225f52ff8cf805d53e8a
tapachec0 <talytapacheco.1c2015@gmail.com>
2018-08-28 10:01:43 -0300

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@ var distanciaTopHeader = $("#header").offset().top; //posição do topo inicial 
 cabeçalho fica com posicao fixa e no topo. Caso contrário, o topo da window é menor, então 
 há conteudo acima e volta ao normal*/
 function fixarHeader() {
-	if($(window).scrollTop() > distanciaTopHeader) {
+	if($(window).scrollTop() >= distanciaTopHeader) {
 		$("#header").addClass("fixed");
 	}
 
@@ -13,7 +13,68 @@ function fixarHeader() {
 	}
 }
 
+var indexLetraPalavra = 0;
+var text = document.getElementById('text').innerText;
+document.getElementById('text').innerText = ""
+
+function typePalavra() {
+	
+	if(indexLetraPalavra < text.length) {
+		document.getElementById('text').innerHTML += text.charAt(indexLetraPalavra);
+		indexLetraPalavra ++;
+		setTimeout(typePalavra, 145);
+	}
+
+	else {
+		document.getElementById('text').style.animation = "blink-caret .75s step-end infinite";
+	}
+
+}
+
+/* Função aprendida no curso de scrollSuave no vídeo gratuito disponibilizado no Youtube pelo
+canal Origamid */
+function scrollSuave(){
+	$('#header nav a').click(function(e){
+		e.preventDefault();
+		var secaoId = $(this).attr('href'),
+			distanciaTopSecao = $(secaoId).offset().top,
+			menuHeight = $('#header').innerHeight();
+			console.log(secaoId);
+			console.log(distanciaTopSecao);
+		$('html, body').animate({
+			scrollTop: distanciaTopSecao - menuHeight //como coloca no topo a secao, o menu cobre, por isso subtrai a altura do menu
+		}, 500);
+	});
+}
+
+/*=============================================================
+| Plugin--> slick.js 
+|
+| License:
+| Copyright (c) 2017 Ken Wheeler
+| Licensed under the MIT license.
+===============================================================*/ 
+
+function slider() {
+	$('.slider').slick({
+		autoplay: true,
+		prevArrow: $(".slick-prev"),
+    	nextArrow: $(".slick-next"),
+		autoplaySpeed: 7000,
+		fade: true,
+		cssEase: 'linear',
+		arrows: true
+	});
+}
+
 //Função que controla o scroll
 window.addEventListener('scroll', function() {
 	fixarHeader();
+	
+});
+
+window.addEventListener('load', function() {
+	typePalavra();
+	scrollSuave();
+	slider();
 });
